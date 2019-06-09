cf3f7d72b08fe9b553b26395c3eb95a84217000c
rsambouncer <rsambouncer@gmail.com>
2019-06-08 19:15:11 -0700

    Update cowbasic.java

--- cowbasic.java
+++ cowbasic.java
@@ -6,8 +6,8 @@ PROG: cowbasic
 import java.io.*;
 import java.util.*;
 
-public class cowbasic{                             
-  
+public class cowbasic
+{                               
   public static String[] varnames = new String[100]; 
     //maximum of 100 variables in problem statement
   public static int[][] codevars = new int[100][62]; 
@@ -139,47 +139,125 @@ public class cowbasic{
     return a;
   }
   
-  private static class Matrix{
+  private static class Matrix
+  {
     public int[][] els;
       //elements of the matrix
     public int h;
       //matrix height
     public int w;
       //matrix width
-    public Matrix(int w,int h){
+    
+    //creates empty matrix with given dimensions
+    public Matrix(int w,int h)
+    {
       els = new int[h][w];
       this.h = h;
       this.w = w;
     }
-    public Matrix(int[][] els){
+    
+    //creates Matrix with the given int[][]
+    public Matrix(int[][] els)
+    {
       this.els = els;
       this.h = els.length;
       this.w = els[0].length;
     }
-    public static Matrix add(Matrix mat1, Matrix mat2){
-      if(mat1.w!=mat2.w||mat1.h!=mat2.h) return null;
-      int[][] sum = new int[mat1.w][mat1.h];
-      for(int a=0;a<mat1.w;a++)for(int b=0;b<mat1.h;b++) 
-        sum[a][b] = mat1.els[a][b]+mat2.els[a][b];
-      return new Matrix(sum);
-    }
-    public static Matrix multiply(Matrix mat1, Matrix mat2){
-      if(mat1.w!=mat2.h) return null;
-      int[][] product = new int[mat1.h][mat2.w];
-      for(int a=0;a<mat1.h;a++) for(int b=0;b<mat2.w;b++){
-        long sum = 0;
-        for(int c=0;c<mat1.w;c++){
-          sum += ((long)mat1.els[a][c])*mat2.els[c][b];
-          sum%= 1_000_000_007;
-        }
-        product[a][b] = (int)sum;
+    
+    public static Matrix identity(int d)
+    {
+      if(d<=0)
+      {
+        throw new RuntimeException("Matrix dimensions have to be positive");
       }
-      return new Matrix(product);
+      int[][] identity = new int[d][d];
+      for(int a=0;a<d;a++)
+      {
+        identity[a][a] = 1;
+      }
+      Matrix identityMatrix = new Matrix(identity);
+      return identityMatrix;
+    }
+    
+    //adds two Matrices, according to the standard formula
+    public static Matrix add(Matrix mat1, Matrix mat2)
+    {
+      if(mat1.w!=mat2.w||mat1.h!=mat2.h)
+      {
+        throw new RuntimeException("Dimensions of matrices have to match to be added");
+      }
+      int[][] sum = new int[mat1.w][mat1.h];
+      for(int a=0;a<mat1.w;a++)
+      {
+        for(int b=0;b<mat1.h;b++)
+        {
+          sum[a][b] = mat1.els[a][b]+mat2.els[a][b];
+          sum[a][b] %= 1_000_000_007;
+          //problem statement specifies that all addition is done mod 10^9+7,
+          //to keep numbers in int range 
+          //10^9+7 is just under half of 2.1 billion
+        }
+      }
+      Matrix answer = new Matrix(sum);
+      return answer;
+    }
+    
+    //subtracts two Matrices, according to the standard formula
+    //subtract(mat1, mat2) is defined as mat1 - mat2
+    public static Matrix subtract(Matrix mat1, Matrix mat2)
+    {
+      if(mat1.w!=mat2.w||mat1.h!=mat2.h)
+      {
+        throw new RuntimeException("Dimensions of matrices have to match to be subtracted");
+      }
+      int[][] diff = new int[mat1.w][mat1.h];
+      for(int a=0;a<mat1.w;a++)
+      {
+        for(int b=0;b<mat1.h;b++)
+        {
+          diff[a][b] = mat1.els[a][b]-mat2.els[a][b];
+          diff[a][b] %= 1_000_000_007;
+          //problem statement specifies that all addition is done mod 10^9+7,
+          //to keep numbers in int range 
+          //10^9+7 is just under half of 2.1 billion
+        }
+      }
+      Matrix answer = new Matrix(diff);
+      return answer;
+    }
+    
+    //multiplies two Matrices, according to the standard formula
+    public static Matrix multiply(Matrix mat1, Matrix mat2)
+    {
+      if(mat1.w!=mat2.h)
+      { 
+        throw new RuntimeException("Inner dimensions of matrices have to match to be multiplied");
+      }
+      int[][] product = new int[mat1.h][mat2.w];
+      for(int a=0;a<mat1.h;a++)
+      {
+        for(int b=0;b<mat2.w;b++)
+        {
+          long sum = 0; //just in case
+          for(int c=0;c<mat1.w;c++)
+          {
+            sum += ((long)mat1.els[a][c])*mat2.els[c][b];
+            sum %= 1_000_000_007;
+            //problem statement specifies that all addition is done mod 10^9+7,
+            //to keep numbers in int range 
+            //10^9+7 is just under half of 2.1 billion
+          }
+          product[a][b] = (int)sum;
+        }
+      }
+      Matrix answer = new Matrix(product);
+      return answer;
     }
     
     //Matrix.power recursively calculates mat raised to the
     //power p by repeated squaring
-    public static Matrix power(Matrix mat, int p){
+    public static Matrix power(Matrix mat, int p)
+    {
       if(mat.w!=mat.h)
       {
         throw new RuntimeException("Only square matrices can be raised to a power");
@@ -191,19 +269,30 @@ public class cowbasic{
       if(p==0) //return identity
       {
         int[][] identity = new int[mat.w][mat.w];
-        for(int a=0;a<mat.w;a++){
+        for(int a=0;a<mat.w;a++)
+        {
           identity[a][a] = 1;
         }
         Matrix identityMatrix = new Matrix(identity);
         return identityMatrix;
       }
-      if(p==1){ 
+      if(p==1)
+      { 
         return mat;
       }
-      Matrix f1Nal = power(mat,p/2);
-      f1Nal = multiply(f1Nal,f1Nal);
-      if(p%2==1) f1Nal = multiply(f1Nal,mat);
-      return f1Nal;
+      if(p%2==0) //even power
+      {
+        Matrix halfPower = power(mat,p/2);
+        Matrix answer = multiply(halfPower,halfPower);
+        return answer;
+      }
+      else
+      {  
+        Matrix halfPower = power(mat,p/2);
+        Matrix powerMinusOne = multiply(halfPower,halfPower);
+        Matrix answer = multiply(powerMinusOne,mat);
+        return answer;
+      }
     }
   }
   
